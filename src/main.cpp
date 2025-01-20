b5f128d319a1813008c3de65279cffd47b9f112e
patelalay231 <patelalay231@gmail.com>
2025-01-21 01:06:23 +0530

    02

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
-
+#include <string>
+using namespace std;
 int main() {
   // Flush after every std::cout / std:cerr
-  // std::cout << std::unitbuf;
-  // std::cerr << std::unitbuf;
+  cout << unitbuf;
+  cerr << unitbuf;
 
-  // // Uncomment this block to pass the first stage
-  std::cout << "$ ";
+  cout << "$ ";
 
-  // std::string input;
-  // std::getline(std::cin, input);
+  string input;
+  getline(cin, input);
+
+  cout << input <<": command not found";
 }
