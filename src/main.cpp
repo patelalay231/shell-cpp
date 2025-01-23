fa202f9bfd9cf4a359813ca592d61d1d639cf99a
patelalay231 <patelalay231@gmail.com>
2025-01-23 22:42:59 +0530

    .codecrafters

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include <string>
+#include <map>
 using namespace std;
 int main() {
   // Flush after every std::cout / std:cerr
   cout << unitbuf;
   cerr << unitbuf;
+
+  map<string,int> mp;
+  mp["echo"] = 1;  
+  mp["exit"] = 1;
+  mp["type"] = 1;
 
   while(true){
     cout << "$ ";
@@ -12,13 +18,23 @@ int main() {
     string input;
     getline(cin, input);
 
-    // exist 0 - program terminates with code/status 0.
+    
+
     string command = input.substr(0,4);
     if(command == "exit"){
       return 0;
     }
     else if(command == "echo"){
       cout << input.substr(5,input.length()) << endl;
+    }
+    else if(command == "type"){
+      string check_command = input.substr(5,input.length());
+      if(mp[check_command]){
+        cout << check_command << " is a shell builtin\n";
+      }
+      else{
+        cout << check_command <<": command not found\n";
+      }
     }
     else{
       cout << input <<": command not found\n";
