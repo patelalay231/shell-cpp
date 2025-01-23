ebc150eba9d3c7861ee25f18d6fb58ad4f1dbc18
patelalay231 <patelalay231@gmail.com>
2025-01-24 01:36:28 +0530

    al

--- your_program.sh
+++ your_program.sh
@@ -24,8 +24,8 @@ set -e # Exit early if any commands fail
 #
 # - Edit this to change how your program runs locally
 # - Edit .codecrafters/run.sh to change how your program runs remotely
-if [ -n "$1" ]; then
-    export PATH="$1"
-    shift
-fi
+# if [ -n "$1" ]; then
+#     export PATH="$1"
+#     shift
+# fi
 exec ./build/shell "$@"
