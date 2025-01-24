eded4f27d5ab23e854789748b999b2ad6339853e
patelalay231 <patelalay231@gmail.com>
2025-01-25 02:07:32 +0530

    .

--- src/main.cpp
+++ src/main.cpp
@@ -63,10 +63,9 @@ int main() {
               int i=0;
               while(i <= msg.length()){
                 while(msg[i] == ' ') i++;
-                string result ="";
+                string result = "";
                 while(msg[i] != ' ') result += msg[i++];
                 cout << result << " ";
-                i+=result.size();
               }
               cout << endl;
             }
