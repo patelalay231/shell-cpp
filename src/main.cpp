fb755e6d196982ed683a5725b22aefc8d5381ca1
patelalay231 <patelalay231@gmail.com>
2025-01-24 11:28:37 +0530

    .

--- src/main.cpp
+++ src/main.cpp
@@ -72,10 +72,9 @@ int main(int argc, char* argv[]) {
     else{
       string get_command_path = isExist(command);
       if(!get_command_path.empty()){
-          string exe_file_path += '/'+ input;
+          string exe_file_path = get_command_path '/'+ input;
           int result = system(exe_file_path);
 
-          // Check the result
           if (result == 0) {
               printf("Program executed successfully.\n");
           } else {
