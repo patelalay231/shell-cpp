437ded3ece985802cc97cd35c54021cf9a0ee648
patelalay231 <patelalay231@gmail.com>
2025-01-24 10:29:38 +0530

    am1

--- src/main.cpp
+++ src/main.cpp
@@ -3,29 +3,47 @@
 #include <map>
 #include <cstdlib>
 #include <sstream>
-#include <sys/stat.h>
+#include <filesystem>
 
-using namespace std;
+using namespace std,filesystem;
 
-void isExist(const char* command) {
-    char* pathEnv = getenv("PATH");
-    if (!pathEnv) return;
+string isExist(const char* command){
+  char* pathEnv = getenv("PATH");
+  if(!pathEnv) return;
 
-    string path(pathEnv);
-    stringstream ss(path);
-    string directory;
+  string path(pathEnv);
+  stringstream ss(path);
 
-    while (getline(ss, directory, ':')) {
-        string fullPath = directory + "/" + command;
-        struct stat sb;
-        if (stat(fullPath.c_str(), &sb) == 0){
-            cout << command << " is " << fullPath << endl;
-            return;
-        }
-      }
-    
-    cout << command <<": not found\n";
+  while(!ss.eof()){
+    getline(ss, path, ':');
+    string fullpath = directory + "/" + command;
+    if(exists(fullpath)){
+      return fullpath;
+    }
+  }
+
+  return "";
 }
+
+// void isExist(const char* command) {
+//     char* pathEnv = getenv("PATH");
+//     if (!pathEnv) return;
+
+//     string path(pathEnv);
+//     stringstream ss(path);
+//     string directory;
+
+//     while (getline(ss, directory, ':')) {
+//         string fullPath = directory + "/" + command;
+//         struct stat sb;
+//         if (stat(fullPath.c_str(), &sb) == 0){
+//             cout << command << " is " << fullPath << endl;
+//             return;
+//         }
+//       }
+    
+//     cout << command <<": not found\n";
+// }
 
 int main(int argc, char* argv[]) {
   // Flush after every std::cout / std:cerr
@@ -57,7 +75,13 @@ int main(int argc, char* argv[]) {
         cout << check_command << " is a shell builtin\n";
       }
       else{
-        isExist(check_command.c_str());
+        string get_command_path = isExist(check_command);
+        if(get_command_path){
+          cout << check_command << " is " << fullPath << endl;
+        }
+        else{
+          cout << command <<": not found\n";
+        }
       }
     }
     else{
