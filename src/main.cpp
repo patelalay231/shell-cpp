22408122decabbb061f12d5850df19c95802fa91
patelalay231 <patelalay231@gmail.com>
2025-01-25 14:40:42 +0530

    .

--- src/main.cpp
+++ src/main.cpp
@@ -66,7 +66,7 @@ int main() {
         }
 
         // Extract the command and arguments
-        string command = input.substr(0, input.find(" "));
+        string command = input.substr(i, input.find(" "));
         input.erase(0, input.find(" ") + 1);
 
         // Handle the "exit" command to break the loop and terminate the program
