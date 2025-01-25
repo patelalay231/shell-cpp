a852595250626aa326eb25855ac9ab7023049159
patelalay231 <patelalay231@gmail.com>
2025-01-25 14:45:22 +0530

    .

--- src/main.cpp
+++ src/main.cpp
@@ -64,10 +64,10 @@ int main() {
             }
             i++; // Skip the closing single quote
         }
-
+        cout << i << endl;
         // Extract the command and arguments
-        string command = input.substr(i, input.find(" "));
-        // input.erase(i, input.find(" ") + 1);
+        string command = input.substr(0, input.find(" "));
+        input.erase(0, input.find(" ") + 1);
 
         // Handle the "exit" command to break the loop and terminate the program
         if (command == "exit") {
@@ -176,7 +176,6 @@ int main() {
         }
         // For other commands, try to find their path and execute them
         else {
-          cout << command << endl;
           string command_path = getFilePath(command);
           if (!command_path.empty()) {
               // Execute the command with arguments
