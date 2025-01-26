13b92849b2a51104e99fcf8dc7443ea0b9fc86c8
patelalay231 <patelalay231@gmail.com>
2025-01-26 11:14:34 +0000

    .

--- src/main.cpp
+++ src/main.cpp
@@ -245,7 +245,7 @@ string readInputNonCanonical() {
             else if(suggestion.size() > 1){
                 if(tabCount == 1){
                     string common_prefix = findCommonPrefix(suggestion);
-                    cout << common_prefix << endl;
+                    input = common_prefix;
                 }
                 else if(tabCount == 2){
                     string matches = "";
